cd665c50f330776c3ef9a3338338f108524dcf36
ShravanSaiyan <shravankumar.sk50@gmail.com>
2023-05-13 19:09:17 +0530

    Added the iterator pattern examples

---
+++ src/iterator/examples/Tester.java
@@ -0,0 +1,23 @@
+package iterator.examples;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Tester {
+    public static void main(String[] args) {
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        // just a demo. We can add a new class that manages adding , deleting menu items
+        menuItems.add(new MenuItem("egg", "An egg", 20));
+        menuItems.add(new MenuItem("mojito", "An refreshing drink", 60));
+
+        Iterator<MenuItem> menuItemIterator = menuItems.iterator();
+
+        menuItemIterator.forEachRemaining(menuItem -> {
+            System.out.println(menuItem.getName());
+            System.out.println(menuItem.getDescription());
+            System.out.println(menuItem.getPrice());
+        });
+    }
+}
